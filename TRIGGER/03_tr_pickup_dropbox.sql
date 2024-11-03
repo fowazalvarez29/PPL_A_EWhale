088490175e6e5caecba33f741efde616e362594d
Your Name <fowaz.213040008@mail.unpas.ac.id>
2024-11-03 12:29:04 +0700

    Memperbaiki Logic Trigger 03

--- TRIGGER/03_tr_pickup_dropbox.sql
+++ TRIGGER/03_tr_pickup_dropbox.sql
@@ -10,7 +10,7 @@ BEGIN
     SET dropbox_status = check_dropbox_status(NEW.dropbox_id);
 
     -- Jika status dropbox adalah 'Drop box is Full!', maka batalkan proses insert
-    IF dropbox_status = 'Drop box is Full!' THEN
+    IF dropbox_status = 'Full' THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Drop box is Full!';
     END IF;
